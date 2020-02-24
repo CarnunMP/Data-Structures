f9932e9adde425285e2105f53e7da35d4100df27
CarnunMP <carnun@hotmail.co.uk>
2020-02-24 14:07:42 +0000

    Gave up on properly importing doubly_linked_list.py, copied it into /queue_and_stack instead.

--- queue_and_stack/dll_queue.py
+++ queue_and_stack/dll_queue.py
@@ -1,20 +1,18 @@
-import sys
-sys.path.append('C:\\Lambda\\CS\\Afternoon Projects\\Data-Structures\\doubly_linked_list')
-print(sys.path)
-from doubly_linked_list.doubly_linked_list import DoublyLinkedList
-
+# import sys
+# sys.path.append('C:\\Lambda\\CS\\Afternoon Projects\\Data-Structures\\doubly_linked_list')
+from doubly_linked_list import DoublyLinkedList
 
 class Queue:
     def __init__(self):
         self.size = 0
         # Why is our DLL a good choice to store our elements?
-        # self.storage = ?
+        self.storage = DoublyLinkedList()
 
     def enqueue(self, value):
-        pass
+        self.storage.add_to_head(value)
 
     def dequeue(self):
-        pass
+        return self.storage.remove_from_tail()
 
     def len(self):
-        pass
+        return self.storage.length
